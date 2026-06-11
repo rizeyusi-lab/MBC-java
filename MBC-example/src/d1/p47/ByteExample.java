3b26086b8cf14c50d1fd3729b53336f8c6b252b2
youngin <cmeos50901@naver.com>
2026-06-11 17:47:20 +0900

    6.11

--- MBC-example/src/d1/p47/ByteExample.java
+++ MBC-example/src/d1/p47/ByteExample.java
@@ -6,13 +6,13 @@ public class ByteExample {
         byte var2 = -30;
         byte var3 = 0;
         byte var4 = 30;
-        byte vsr5 = 127;
-//        byte var6 = 128;
+        byte var5 = 127;
+        int var6 = 128;
 
-        System.out,ptintln(var1);
-        System.out,ptintln(var2);
-        System.out,ptintln(var3);
-        System.out,ptintln(var4);
-        System.out,ptintln(var5);
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
     }
 }

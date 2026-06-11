b59fa204a84dbb1df5be115d36b880e9e1147390
youngin <cmeos50901@naver.com>
2026-06-11 10:49:45 +0900

    예시 작성

---
+++ MBC-example/src/d1/p41/VariableExample.java
@@ -0,0 +1,11 @@
+package d1.p41;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        int value = 10;
+        int result = value + 10;
+
+        System.out.println(value);
+        System.out.println(result);
+    }
+}
---
+++ MBC-example/src/d1/p47/ByteExample.java
@@ -0,0 +1,18 @@
+package d1.p47;
+
+public class ByteExample {
+    public static void main(String[] args) {
+        byte var1 = -128;
+        byte var2 = -30;
+        byte var3 = 0;
+        byte var4 = 30;
+        byte vsr5 = 127;
+//        byte var6 = 128;
+
+        System.out,ptintln(var1);
+        System.out,ptintln(var2);
+        System.out,ptintln(var3);
+        System.out,ptintln(var4);
+        System.out,ptintln(var5);
+    }
+}

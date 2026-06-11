cbdcaece5c726e2b1d39ae8e369ffb85e70c3821
youngin <cmeos50901@naver.com>
2026-06-11 17:47:27 +0900

    6.11

---
+++ MBC-example/src/d1/p62/OperatorExample.java
@@ -0,0 +1,12 @@
+package d1.p62;
+
+public class OperatorExample {
+    public static void main(String[] args) {
+        short s = 100;
+//         short result = -s;    // compile error!
+        int result2 = -s;
+
+        System.out.println(s);
+        System.out.println(result2);
+    }
+}
---
+++ MBC-jungol/src/q9009/Main.java
@@ -0,0 +1,11 @@
+package q9009;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        char b = 'A';
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}

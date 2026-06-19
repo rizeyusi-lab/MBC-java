cc031166e56cb2e67860e37dadd51ce73f1c4a38
na <rizeyusi@gmail.com>
2026-06-19 18:42:13 +0900

    Add files via upload

---
+++ MBC-example/src/d4/p69/Main.java
@@ -0,0 +1,40 @@
+package d4.p69;
+
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double num = 1234567.89;
+
+        DecimalFormat df = new DecimalFormat("0");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("0.0");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("00000000.00000");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("#");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("########.########");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("#,###");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("#,###.##");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("0%");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("#.##%");
+        System.out.println(df.format(num));
+
+        df = new DecimalFormat("₩#,###");
+        System.out.println(df.format(num));
+    }
+}
